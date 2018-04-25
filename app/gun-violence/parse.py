3be4f4347cfed6f4633904c68b974a9130a64d34
jkatzeff <jkatzeff@ucsc.edu>
2018-04-24 21:08:55 -0700

    Modularized

--- app/gun-violence/parse.py
+++ app/gun-violence/parse.py
@@ -2,3 +2,6 @@
  3 files changed, 384 insertions(+), 1 deletion(-)
  create mode 100644 app/gun-violence/makeMap.js
  create mode 100644 app/gun-violence/test2.html
+[master f8d079b] Modularized
+ 1 file changed, 4 insertions(+)
+ create mode 100644 app/gun-violence/parse.py
