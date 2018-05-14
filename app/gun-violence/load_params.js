75204f86fe6ff4ed5b8757d596faf3f6e287ebe3
Diego <dgarci34@ucsc.edu>
2018-05-13 20:59:39 -0700

    give it filename and args to run from the scripts folder

--- app/gun-violence/load_params.js
+++ app/gun-violence/load_params.js
@@ -1,6 +1,20 @@
-var pythonShell = require('python-shell');
+var PythonShell = require('python-shell');
 
-pythonShell.run('scripts/simple_hello.py',function(err){
-    if(err) throw err;
-    console.log('finished');
-});
+//use this function to run a python script from our scripts folder
+function runPyScript(fileName, list){
+
+    var options = {
+        mode: 'text',
+        scriptPath: 'scripts/',
+        args: list
+    };
+ 
+    PythonShell.run(fileName, options, function (err, results) {
+        if (err) throw err;
+        // results is an array consisting of messages collected during execution
+        console.log('results: %j', results);
+    });
+}
+
+runPyScript('simple_hello.py', []);
+
