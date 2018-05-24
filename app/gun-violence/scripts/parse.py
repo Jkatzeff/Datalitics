3907e2e0d7b320bbf8cbb361cdf0ea6d4f38372d
Brian Liu <bliu9883@gmail.com>
2018-05-24 15:33:50 -0700

    commented out part of slider code that bugged our program, mb guys

--- app/gun-violence/scripts/parse.py
+++
@@ -1,9 +0,0 @@
-[master c466379] Modularized
- 3 files changed, 384 insertions(+), 1 deletion(-)
- create mode 100644 app/gun-violence/makeMap.js
- create mode 100644 app/gun-violence/test2.html
-[master f8d079b] Modularized
- 1 file changed, 4 insertions(+)
- create mode 100644 app/gun-violence/parse.py
-[master 3be4f43] Modularized
- 2 files changed, 3 insertions(+), 90 deletions(-)
